e686cb238c8c0279521c28db8cd6b163a7d78405
Dillon Carter <73437822+PickleMustard@users.noreply.github.com>
2020-10-30 16:39:31 -0500

    Puzzle's Included in Created Scenes

---
+++ New Unity Project/Assets/Scenes/Scripts/Elements/Element.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Element : MonoBehaviour
+{
+    //What type of element
+    public ElementHolder.elements element;
+    private Rigidbody2D rigid;
+    private SpriteRenderer spriteRenderer;
+
+    public void Start ( ) {
+        gameObject.tag = "Element";
+        rigid = GetComponent<Rigidbody2D> ( );
+        spriteRenderer = GetComponent<SpriteRenderer> ( );
+    }
+
+    public ElementHolder.elements getElement ( ) {
+        return element;
+    }
+
+    //Destroy on impact with player
+    public void destroyThis ( ) {
+        spriteRenderer.sprite = null;
+        Destroy ( this );
+    }
+
+}
